f78cde37e1e7b37ab0e8c5952daaa37bb33ea1cc
alpha-zeta <anishmajhi05@gmail.com>
2020-03-23 19:34:47 +0530

    other things

--- date.js
+++ date.js
@@ -1,10 +1,13 @@
 module.exports = getDate;
 function getDate(x) {
 	let date = new Date();
-
-	let intlDateObj = new Intl.DateTimeFormat('en-GB', {
+	let options = {
+		year     : 'numeric',
+		month    : 'short',
+		day      : 'numeric',
 		timeZone : x
-	});
+	};
+	let intlDateObj = new Intl.DateTimeFormat('en-GB', options);
 
 	let localTime = intlDateObj.format(date);
 	return localTime;
